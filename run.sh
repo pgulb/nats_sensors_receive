a7fb921797680f32f35b490b61343f4524787199
Piotr Gulbinowicz <piotr.gulbinowicz@imgw.pl>
2025-02-17 19:05:06 +0100

    add script

---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd /root/nats_receive
+docker rm -f nats_receive
+docker pull ghcr.io/pgulb/nats_sensors_receive:main
+docker run -d --name nats_receive -v ./credentials.json:/app/credentials.json:ro -v ./nats.creds:/app/nats.creds:ro ghcr.io/pgulb/nats_sensors_receive:main
