25e96d7a5650f84049a637410f044b5353c6ba93
Piotr Gulbinowicz <piotr.gulbinowicz@imgw.pl>
2025-02-18 10:10:42 +0100

    add image remove

--- run.sh
+++ run.sh
@@ -2,5 +2,6 @@
 
 cd /root/nats_receive
 docker rm -f nats_receive
+docker image rm ghcr.io/pgulb/nats_sensors_receive:main
 docker pull ghcr.io/pgulb/nats_sensors_receive:main
 docker run -d --name nats_receive -v ./credentials.json:/app/credentials.json:ro -v ./nats.creds:/app/nats.creds:ro ghcr.io/pgulb/nats_sensors_receive:main
